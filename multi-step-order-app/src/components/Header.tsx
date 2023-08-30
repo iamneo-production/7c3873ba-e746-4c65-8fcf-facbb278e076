8e8464b9d15a510bab5cf3f5772641066e00da0b
heymeowcat <heyvidura@gmail.com>
2023-08-30 20:06:09 +0000

    fixed: login fixed with jwt token

--- multi-step-order-app/src/components/Header.tsx
+++ multi-step-order-app/src/components/Header.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Nav, Button, Dropdown } from 'react-bootstrap';
-import AuthModal from './AuthModal'
+import AuthModal from './AuthModal';
 import LoginForm from './LoginForm';
 import RegisterForm from './RegisterForm';
 
@@ -8,54 +8,12 @@ interface HeaderProps {
   username: string | null;
   onLogout: () => void;
   onLogin: (email: string, password: string) => void;
-  onRegister: (username: string, email: string, password: string) => void;
+  onRegister: (email: string, password: string) => void;
 }
 
 function Header({ username, onLogout, onLogin, onRegister }: HeaderProps) {
   const [showAuthModal, setShowAuthModal] = useState(false);
   const [isLoginFormShown, setIsLoginFormShown] = useState(true);
-
-  const handleLogin = async (email: string, password: string) => {
-    try {
-      const response = await fetch('https://ide-cdccbbbcebbacdbfaeaaecfbcdbacecdeab.premiumproject.examly.io/proxy/8000/users/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email, password }),
-      });
-
-      if (response.ok) {
-        console.log('Login successful');
-        setShowAuthModal(false);
-      } else {
-        console.error('Login failed');
-      }
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  };
-
-  const handleRegister = async (email: string, password: string) => {
-    try {
-      const response = await fetch('https://ide-cdccbbbcebbacdbfaeaaecfbcdbacecdeab.premiumproject.examly.io/proxy/8000/users/register', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email, password }),
-      });
-
-      if (response.ok) {
-        console.log('Registration successful');
-        setShowAuthModal(false);
-      } else {
-        console.error('Registration failed');
-      }
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  };
 
   const handleAuthModalClose = () => {
     setShowAuthModal(false);
@@ -94,9 +52,9 @@ function Header({ username, onLogout, onLogin, onRegister }: HeaderProps) {
       </nav>
       <AuthModal show={showAuthModal} onHide={handleAuthModalClose}>
         {isLoginFormShown ? (
-          <LoginForm onLogin={handleLogin} onClose={handleAuthModalClose} onSwitch={handleSwitchForm} />
+          <LoginForm onLogin={onLogin} onClose={handleAuthModalClose} onSwitch={handleSwitchForm} />
         ) : (
-          <RegisterForm onRegister={handleRegister} onClose={handleAuthModalClose} onSwitch={handleSwitchForm} />
+          <RegisterForm onRegister={onRegister} onClose={handleAuthModalClose} onSwitch={handleSwitchForm} />
         )}
       </AuthModal>
     </header>
--- server/routes/users.mjs
+++ server/routes/users.mjs
@@ -2,46 +2,43 @@ import express from "express";
 import db from "../db/conn.mjs";
 import { ObjectId } from "mongodb";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
+const JWT_SECRET = "temp_secret_key";
 
-// This section will help you get a list of all users.
+
 router.get("/", async (req, res) => {
   let collection = await db.collection("users");
   let results = await collection.find({}).toArray();
   res.send(results).status(200);
 });
 
-// This section will help you get a single user by id
 router.get("/:id", async (req, res) => {
   let collection = await db.collection("users");
-  let query = {_id: new ObjectId(req.params.id)};
+  let query = { _id: new ObjectId(req.params.id) };
   let result = await collection.findOne(query);
 
   if (!result) res.send("User not found").status(404);
   else res.send(result).status(200);
 });
 
-// This section will help you create a new user.
 router.post("/", async (req, res) => {
   let newUser = {
     username: req.body.username,
     email: req.body.email,
-    // Add other user fields as needed
   };
   let collection = await db.collection("users");
   let result = await collection.insertOne(newUser);
   res.send(result).status(201);
 });
 
-// This section will help you update a user by id.
 router.patch("/:id", async (req, res) => {
   const query = { _id: new ObjectId(req.params.id) };
-  const updates =  {
+  const updates = {
     $set: {
       username: req.body.username,
       email: req.body.email,
-      // Update other user fields as needed
     }
   };
 
@@ -50,7 +47,6 @@ router.patch("/:id", async (req, res) => {
   res.send(result).status(200);
 });
 
-// This section will help you delete a user
 router.delete("/:id", async (req, res) => {
   const query = { _id: new ObjectId(req.params.id) };
 
@@ -60,25 +56,20 @@ router.delete("/:id", async (req, res) => {
   res.send(result).status(200);
 });
 
-// This section will help you register a new user.
 router.post('/register', async (req, res) => {
   try {
     const { username, email, password } = req.body;
 
-    // Check if user with the provided email already exists
     const existingUser = await db.collection('users').findOne({ email });
     if (existingUser) {
       return res.status(400).json({ message: 'User already exists with this email' });
     }
-
-    // Hash the password before storing
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const newUser = {
       username,
       email,
       password: hashedPassword,
-      // Add other user fields as needed
     };
 
     const collection = await db.collection('users');
@@ -91,24 +82,24 @@ router.post('/register', async (req, res) => {
   }
 });
 
-// This section will help you authenticate a user.
 router.post('/login', async (req, res) => {
   try {
     const { email, password } = req.body;
 
     const user = await db.collection('users').findOne({ email });
+
     if (!user) {
       return res.status(401).json({ message: 'Invalid credentials' });
     }
-
-    // Compare the provided password with the hashed password
     const passwordMatch = await bcrypt.compare(password, user.password);
 
     if (!passwordMatch) {
       return res.status(401).json({ message: 'Invalid credentials' });
     }
+    
+    const token = jwt.sign({ userId: user._id }, JWT_SECRET);
+    res.status(200).json({ message: 'Login successful', token, userId: user._id });
 
-    res.status(200).json({ message: 'Login successful', userId: user._id });
   } catch (error) {
     console.error('Error:', error);
     res.status(500).send('Internal Server Error');
