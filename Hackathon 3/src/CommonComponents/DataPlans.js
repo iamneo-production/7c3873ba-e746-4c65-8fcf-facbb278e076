4f9561692b5efbe13a8444ed12b7d40eacc370a1
Vidura Fernando <heyvidura@gmail.com>
2023-08-31 17:59:35 +0530

    fix: DataPlan fixed to add more data, quota filtering, added responsivenes

--- Hackathon 3/src/CommonComponents/DataPlans.js
+++ Hackathon 3/src/CommonComponents/DataPlans.js
@@ -28,20 +28,21 @@ export const DataPlans = () => {
   function valuetext(value) {
     return `${value} GB`;
   }
-  const [filter, setFilter] = useState("");
+  const [filter, setFilter] = useState(0);
 
   const filterdData = allDatapacks.filter((item) => {
-    return item.title == filter ? item.title : filter == 0 && allDatapacks;
+    return item.quota >= filter || filter === 0;
   });
 
   return (
-    <>
-      <Box sx={{ width: 300 }}>
+    <Box>
+      <Box sx={{ width: "100%", maxWidth: 300, margin: "auto" }}>
         <Typography variant="subtitle1" fontWeight="bold">
-          GB Range
+          Quota Range
         </Typography>
         <Slider
-          onChange={(value) => setFilter(value.target.value)}
+          value={filter}
+          onChange={(event, value) => setFilter(value)}
           aria-label="Small steps"
           defaultValue={0}
           getAriaValueText={valuetext}
@@ -52,27 +53,39 @@ export const DataPlans = () => {
           valueLabelDisplay="auto"
         />
       </Box>
-      <Grid container>
-        {!loading ? (
-          <>
-            {filterdData.length >= 1 ? (
-              <>
-                {filterdData.map((data, index) => {
-                  return (
-                    <Grid key={index} item xs={6} sm={4} md={2}>
-                      <PackageCard type="data" data={data} />
-                    </Grid>
-                  );
-                })}
-              </>
-            ) : (
-              <>No Packages available</>
-            )}
-          </>
-        ) : (
-          <div>Loading</div>
-        )}
-      </Grid>
-    </>
+      <Box display="flex" justifyContent="center">
+        <Grid container spacing={2}>
+          {!loading ? (
+            <>
+              {filterdData.length >= 1 ? (
+                <>
+                  {filterdData.map((data, index) => {
+                    return (
+                      <Grid key={index} item xs={12} sm={6} md={4}>
+                        <PackageCard type="data" data={data} />
+                      </Grid>
+                    );
+                  })}
+                </>
+              ) : (
+                <Box
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="center"
+                  width="100%"
+                  height="200px"
+                  fontSize="20px"
+                  fontWeight="bold"
+                >
+                  No Packages available
+                </Box>
+              )}
+            </>
+          ) : (
+            <div>Loading</div>
+          )}
+        </Grid>
+      </Box>
+    </Box>
   );
 };
